7e1ae3522404e360443f05a084026afd6f820d3a
Michael Tidwell <tidwell@zebedee.io>
2021-03-27 20:13:01 -0400

    colyseus and generic frontend running

---
+++ src/server/index.ts
@@ -0,0 +1,21 @@
+import http from 'http'
+import express from 'express'
+import cors from 'cors'
+import { Server } from 'colyseus'
+import { monitor } from '@colyseus/monitor'
+
+const port = Number(process.env.PORT || 2567)
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const server = http.createServer(app)
+const gameServer = new Server({
+  server,
+})
+
+app.use('/colyseus', monitor())
+
+gameServer.listen(port)
+console.log(`Listening on ws://host:${port}`)
